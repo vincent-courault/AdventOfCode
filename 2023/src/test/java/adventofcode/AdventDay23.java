475ac88dcee182bc5dabc5217f58c6d9d2c4334d
Vincent <vincent.courault@gmail.com>
2024-01-04 23:52:01 +0100

    J23 : améliore la partie 1

--- 2023/src/test/java/adventofcode/AdventDay23.java
+++ 2023/src/test/java/adventofcode/AdventDay23.java
@@ -16,27 +16,16 @@ public class AdventDay23 extends Commun {
     @Test
     public void etape1_exemple() throws URISyntaxException, IOException {
         List<String> inputs = lectureDuFichier(this, true);
-        assertEquals(94, traitement(inputs, true));
+        assertEquals(94, traitement(inputs));
     }
 
     @Test
     public void etape1() throws IOException, URISyntaxException {
         List<String> inputs = lectureDuFichier(this, false);
-        assertEquals(2366, traitement(inputs, true));
+        assertEquals(2366, traitement(inputs));
     }
 
-    @Test
-    public void etape2_exemple() throws URISyntaxException, IOException {
-        List<String> inputs = lectureDuFichier(this, true);
-        assertEquals(154, traitement(inputs, false));
-    }
-
-    public void etape2() throws URISyntaxException, IOException {
-        List<String> inputs = lectureDuFichier(this, false);
-        assertEquals(154, traitement(inputs, false));
-    }
-
-    public long traitement(List<String> inputs, boolean etape1) {
+    public long traitement(List<String> inputs) {
         int y = 0;
         long resultat;
         for (String line : inputs) {
@@ -48,42 +37,41 @@ public class AdventDay23 extends Commun {
         Point depart = new Point(1, 0);
         Point fin = new Point(carte.max_x - 1, carte.max_y);
 
-        parcoursLaCarte(depart, fin, etape1);
+        parcoursLaCarte(depart, fin);
         resultat = max;
         System.out.println(this.getClass().getSimpleName() + " " + name + " : " + resultat);
         return resultat;
     }
 
-    public void parcoursLaCarte(Point depart, Point fin, boolean etape1) {
-        if (etape1) {
-            State departS = new State(depart, fin, new HashSet<>(), 0);
-            LinkedList<State> queue = new LinkedList<>(List.of(departS));
-            while (!queue.isEmpty()) {
-                State etat = queue.pollFirst();
-                queue.addAll(etat.next());
-            }
-        } else {
-            StateP2 departS = new StateP2(fin, depart, new HashSet<>(), 0);
-            LinkedList<StateP2> queue = new LinkedList<>(List.of(departS));
-            while (!queue.isEmpty()) {
-                StateP2 etat = queue.pollFirst();
-                queue.addAll(etat.next());
-            }
+    public void parcoursLaCarte(Point depart, Point fin) {
+        State departS = new State(depart, fin, new TreeSet<>(), 0);
+        List<State> queue = new ArrayList<>(List.of(departS));
+        while (!queue.isEmpty()) {
+            State etat = queue.removeFirst();
+            queue.addAll(etat.next());
         }
     }
 
-    public static class Point {
+    public static class Point implements Comparable<Point> {
         final long x, y;
+
         public Point(long x, long y) {
             this.x = x;
             this.y = y;
         }
+
         @Override
         public String toString() {
             return x + "," + y;
         }
+
         public Point add(Point p) {
             return new Point(x + p.x, y + p.y);
+        }
+
+        @Override
+        public int compareTo(Point o) {
+            return o.toString().compareTo(this.toString());
         }
     }
 
@@ -96,12 +84,12 @@ public class AdventDay23 extends Commun {
 
     public static class Map2D {
         private final Character default_value;
-        private final HashMap<Long, HashMap<Long, Character>> map;
+        private final TreeMap<Long, TreeMap<Long, Character>> map;
         protected long max_x, max_y;
 
         public Map2D(Character def) {
             this.default_value = def;
-            map = new HashMap<>();
+            map = new TreeMap<>();
         }
 
         public Character get(long x, long y) {
@@ -111,7 +99,7 @@ public class AdventDay23 extends Commun {
         }
 
         public void set(long x, long y, Character val) {
-            map.computeIfAbsent(x, _ -> new HashMap<>());
+            map.computeIfAbsent(x, _ -> new TreeMap<>());
             map.get(x).put(y, val);
             max_x = Math.max(max_x, x);
             max_y = Math.max(max_y, y);
@@ -126,9 +114,9 @@ public class AdventDay23 extends Commun {
         Point position;
         Point fin;
         Integer nbSteps;
-        HashSet<String> visited;
+        Set<Point> visited;
 
-        public State(Point position, Point fin, HashSet<String> visited, Integer nbSteps) {
+        public State(Point position, Point fin, Set<Point> visited, Integer nbSteps) {
             this.position = position;
             this.nbSteps = nbSteps;
             this.fin = fin;
@@ -146,83 +134,36 @@ public class AdventDay23 extends Commun {
                 return etatsSuivants;
             }
 
-            visited.add(position.toString());
-            new HashSet<>(visited);
+            visited.add(position);
             if (carte.get(position).equals('.')) {
                 Point pointSuivantN = position.add(Direction.N);
-                if (!carte.get(pointSuivantN).equals('#') && !visited.contains(pointSuivantN.toString()))
-                    etatsSuivants.add(new State(pointSuivantN, fin, new HashSet<>(visited), nbSteps + 1));
+                if (!visited.contains(pointSuivantN) && !carte.get(pointSuivantN).equals('#'))
+                    etatsSuivants.add(new State(pointSuivantN, fin, new TreeSet<>(visited), nbSteps + 1));
                 Point pointSuivantS = position.add(Direction.S);
-                if (!carte.get(pointSuivantS).equals('#') && !visited.contains(pointSuivantS.toString()))
-                    etatsSuivants.add(new State(pointSuivantS, fin, new HashSet<>(visited), nbSteps + 1));
+                if (!visited.contains(pointSuivantS) && !carte.get(pointSuivantS).equals('#'))
+                    etatsSuivants.add(new State(pointSuivantS, fin, new TreeSet<>(visited), nbSteps + 1));
                 Point pointSuivantE = position.add(Direction.E);
-                if (!carte.get(pointSuivantE).equals('#') && !visited.contains(pointSuivantE.toString()))
-                    etatsSuivants.add(new State(pointSuivantE, fin, new HashSet<>(visited), nbSteps + 1));
+                if (!visited.contains(pointSuivantE) && !carte.get(pointSuivantE).equals('#'))
+                    etatsSuivants.add(new State(pointSuivantE, fin, new TreeSet<>(visited), nbSteps + 1));
                 Point pointSuivantO = position.add(Direction.O);
-                if (!carte.get(pointSuivantO).equals('#') && !visited.contains(pointSuivantO.toString()))
-                    etatsSuivants.add(new State(pointSuivantO, fin, new HashSet<>(visited), nbSteps + 1));
+                if (!visited.contains(pointSuivantO) && !carte.get(pointSuivantO).equals('#'))
+                    etatsSuivants.add(new State(pointSuivantO, fin, new TreeSet<>(visited), nbSteps + 1));
             }
             if (carte.get(position).equals('>')) {
                 Point pointSuivantE = position.add(Direction.E);
-                if (!carte.get(pointSuivantE).equals('#') && !visited.contains(pointSuivantE.toString()))
-                    etatsSuivants.add(new State(pointSuivantE, fin, new HashSet<>(visited), nbSteps + 1));
+                if (!visited.contains(pointSuivantE) && !carte.get(pointSuivantE).equals('#'))
+                    etatsSuivants.add(new State(pointSuivantE, fin, new TreeSet<>(visited), nbSteps + 1));
 
             }
             if (carte.get(position).equals('v')) {
                 Point pointSuivantS = position.add(Direction.S);
-                if (!carte.get(pointSuivantS).equals('#') && !visited.contains(pointSuivantS.toString()))
-                    etatsSuivants.add(new State(pointSuivantS, fin, new HashSet<>(visited), nbSteps + 1));
+                if (!visited.contains(pointSuivantS) && !carte.get(pointSuivantS).equals('#'))
+                    etatsSuivants.add(new State(pointSuivantS, fin, new TreeSet<>(visited), nbSteps + 1));
             }
 
             return etatsSuivants;
         }
     }
 
-    public class StateP2 {
-
-        Point position;
-        Point fin;
-        Integer nbSteps;
-        HashSet<String> visited;
-
-        public StateP2(Point position, Point fin, HashSet<String> visited, Integer nbSteps) {
-            this.position = position;
-            this.nbSteps = nbSteps;
-            this.fin = fin;
-            this.visited = visited;
-        }
-
-        public String toString() {
-            return position.toString() + "/" + nbSteps.toString();
-        }
-
-        public List<StateP2> next() {
-            List<StateP2> etatsSuivants = new ArrayList<>();
-            if (position.x == fin.x && position.y == fin.y) {
-                max = Math.max(max, nbSteps);
-                return etatsSuivants;
-            }
-            HashSet<String> newVisited = new HashSet<>(visited);
-            newVisited.add(position.toString());
-            if (!carte.get(position).equals('#') && position.x >= 0 && position.y >= 0 && position.x <=
-            carte.max_x && position.y <= carte.max_y){
-                Point pointSuivantN = position.add(Direction.N);
-                if (!visited.contains(pointSuivantN.toString()))
-                    etatsSuivants.add(new StateP2(pointSuivantN, fin, newVisited, nbSteps + 1));
-                Point pointSuivantS = position.add(Direction.S);
-                if (!visited.contains(pointSuivantS.toString()))
-                    etatsSuivants.add(new StateP2(pointSuivantS, fin, newVisited, nbSteps + 1));
-                Point pointSuivantE = position.add(Direction.E);
-                if (!visited.contains(pointSuivantE.toString()))
-                    etatsSuivants.add(new StateP2(pointSuivantE, fin, newVisited, nbSteps + 1));
-                Point pointSuivantO = position.add(Direction.O);
-                if (!visited.contains(pointSuivantO.toString()))
-                    etatsSuivants.add(new StateP2(pointSuivantO, fin, newVisited, nbSteps + 1));
-            }
-
-
-            return etatsSuivants;
-        }
-    }
 }
 
