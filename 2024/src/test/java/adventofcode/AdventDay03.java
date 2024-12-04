fd4cc6a5f3bdc791dc227054a7f528ca4d874ef7
Vincent <vincent.courault@gmail.com>
2024-12-04 07:20:24 +0100

    ajoute j3 et j4

--- 2024/src/test/java/adventofcode/AdventDay03.java
+++ 2024/src/test/java/adventofcode/AdventDay03.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -14,30 +16,59 @@ public class AdventDay03 extends Commun {
     @Test
     public void etape1_exemple() throws URISyntaxException, IOException {
         List<String> inputs = lectureDuFichier(this, true);
-        assertEquals(0, traitement(inputs));
+        assertEquals(161, traitement(inputs));
     }
 
     @Test
     public void etape1() throws IOException, URISyntaxException {
         List<String> inputs = lectureDuFichier(this, false);
-        assertEquals(0, traitement(inputs));
+        assertEquals(187825547, traitement(inputs));
     }
 
     @Test
     public void etape2_exemple() throws URISyntaxException, IOException {
         List<String> inputs = lectureDuFichier(this, true);
-        assertEquals(0, traitement(inputs));
+        assertEquals(48, traitement2(inputs));
     }
 
     @Test
     public void etape2() throws IOException, URISyntaxException {
         List<String> inputs = lectureDuFichier(this, false);
-        assertEquals(0, traitement(inputs));
+        assertEquals(85508223, traitement2(inputs));
     }
 
     public int traitement(List<String> inputs) {
         int resultat = 0;
+        Pattern pattern = Pattern.compile("mul\\((\\d+),(\\d+)\\)");
+        for (String input : inputs) {
+            Matcher matcher = pattern.matcher(input);
+            while (matcher.find()) {
+                resultat += Integer.parseInt(matcher.group(1)) * Integer.parseInt(matcher.group(2));
+            }
+        }
+        System.out.println(this.getClass().getSimpleName() + " " + name + " : " + resultat);
+        return resultat;
+    }
 
+
+    public int traitement2(List<String> inputs) {
+        int resultat = 0;
+        boolean compte = true;
+        Pattern pattern = Pattern.compile("mul\\((\\d+),(\\d+)\\)|do\\(\\)|don\\'t\\(\\)");
+        for (String input : inputs) {
+            Matcher matcher = pattern.matcher(input);
+            while (matcher.find()) {
+                if (matcher.group().startsWith("mul")) {
+                    if (compte) {
+                        resultat += Integer.parseInt(matcher.group(1)) * Integer.parseInt(matcher.group(2));
+                    }
+                } else if (matcher.group().equals("do()")) {
+                    compte = true;
+                } else if (matcher.group().equals("don't()")) {
+                    compte = false;
+                }
+            }
+        }
         System.out.println(this.getClass().getSimpleName() + " " + name + " : " + resultat);
         return resultat;
     }
--- 2024/src/test/java/adventofcode/AdventDay04.java
+++ 2024/src/test/java/adventofcode/AdventDay04.java
@@ -11,35 +11,164 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class AdventDay04 extends Commun {
 
 
+    static int cherche2MAS(Grid<Character> grille, int ligne, int colonne) {
+        int mas = 0;
+        if (grille.isValid(ligne - 1, colonne - 1)
+                && grille.isValid(ligne + 1, colonne + 1)) {
+            // les 2 M en haut
+            if (grille.get(ligne - 1, colonne - 1) == 'M' &&
+                    grille.get(ligne - 1, colonne + 1) == 'M' &&
+                    grille.get(ligne + 1, colonne - 1) == 'S' &&
+                    grille.get(ligne + 1, colonne + 1) == 'S') {
+                mas++;
+            }
+            // Ms à gauche
+            if (grille.get(ligne - 1, colonne - 1) == 'M' &&
+                    grille.get(ligne - 1, colonne + 1) == 'S' &&
+                    grille.get(ligne + 1, colonne - 1) == 'M' &&
+                    grille.get(ligne + 1, colonne + 1) == 'S') {
+                mas++;
+            }
+            // M en bas
+            if (grille.get(ligne - 1, colonne - 1) == 'S' &&
+                    grille.get(ligne - 1, colonne + 1) == 'S' &&
+                    grille.get(ligne + 1, colonne - 1) == 'M' &&
+                    grille.get(ligne + 1, colonne + 1) == 'M') {
+                mas++;
+            }
+            // M à droite
+            if (grille.get(ligne - 1, colonne - 1) == 'S' &&
+                    grille.get(ligne - 1, colonne + 1) == 'M' &&
+                    grille.get(ligne + 1, colonne - 1) == 'S' &&
+                    grille.get(ligne + 1, colonne + 1) == 'M') {
+                mas++;
+            }
+        }
+        return mas;
+    }
+
+    static int chercheXMAS(Grid<Character> grille, int ligne, int colonne) {
+        int xmas = 0;
+        // Vers le haut
+        if (grille.isValid(ligne - 3, colonne)) {
+            if (grille.get(ligne - 1, colonne) == 'M' &&
+                    grille.get(ligne - 2, colonne) == 'A' &&
+                    grille.get(ligne - 3, colonne) == 'S') {
+                xmas++;
+            }
+        }
+        // Vers le bas
+        if (grille.isValid(ligne + 3, colonne)) {
+            if (grille.get(ligne + 1, colonne) == 'M' &&
+                    grille.get(ligne + 2, colonne) == 'A' &&
+                    grille.get(ligne + 3, colonne) == 'S') {
+                xmas++;
+            }
+        }
+        // Vers la gauche
+        if (grille.isValid(ligne, colonne - 3)) {
+            if (grille.get(ligne, colonne - 1) == 'M' &&
+                    grille.get(ligne, colonne - 2) == 'A' &&
+                    grille.get(ligne, colonne - 3) == 'S') {
+                xmas++;
+            }
+        }
+        // Vers la droite
+        if (grille.isValid(ligne, colonne + 3)) {
+            if (grille.get(ligne, colonne + 1) == 'M' &&
+                    grille.get(ligne, colonne + 2) == 'A' &&
+                    grille.get(ligne, colonne + 3) == 'S') {
+                xmas++;
+            }
+        }
+        // En haut à gauche
+        if (grille.isValid(ligne - 3, colonne - 3)) {
+            if (grille.get(ligne - 1, colonne - 1) == 'M' &&
+                    grille.get(ligne - 2, colonne - 2) == 'A' &&
+                    grille.get(ligne - 3, colonne - 3) == 'S') {
+                xmas++;
+            }
+        }
+        // En haut à droite
+        if (grille.isValid(ligne - 3, colonne + 3)) {
+            if (grille.get(ligne - 1, colonne + 1) == 'M' &&
+                    grille.get(ligne - 2, colonne + 2) == 'A' &&
+                    grille.get(ligne - 3, colonne + 3) == 'S') {
+                xmas++;
+            }
+        }
+        // En bas à droite
+        if (grille.isValid(ligne + 3, colonne + 3)) {
+            if (grille.get(ligne + 1, colonne + 1) == 'M' &&
+                    grille.get(ligne + 2, colonne + 2) == 'A' &&
+                    grille.get(ligne + 3, colonne + 3) == 'S') {
+                xmas++;
+            }
+        }
+        // En bas à gauche
+        if (grille.isValid(ligne + 3, colonne - 3)) {
+            if (grille.get(ligne + 1, colonne - 1) == 'M' &&
+                    grille.get(ligne + 2, colonne - 2) == 'A' &&
+                    grille.get(ligne + 3, colonne - 3) == 'S') {
+                xmas++;
+            }
+        }
+        return xmas;
+    }
+
     @Test
     public void etape1_exemple() throws URISyntaxException, IOException {
         List<String> inputs = lectureDuFichier(this, true);
-        assertEquals(0, traitement(inputs));
+        assertEquals(18, traitement(inputs));
     }
 
     @Test
     public void etape1() throws IOException, URISyntaxException {
         List<String> inputs = lectureDuFichier(this, false);
-        assertEquals(0, traitement(inputs));
+        assertEquals(2464, traitement(inputs));
     }
 
     @Test
     public void etape2_exemple() throws URISyntaxException, IOException {
         List<String> inputs = lectureDuFichier(this, true);
-        assertEquals(0, traitement(inputs));
+        assertEquals(9, traitement2(inputs));
     }
 
     @Test
     public void etape2() throws IOException, URISyntaxException {
         List<String> inputs = lectureDuFichier(this, false);
-        assertEquals(0, traitement(inputs));
+        assertEquals(1982, traitement2(inputs));
     }
 
     public int traitement(List<String> inputs) {
         int resultat = 0;
+        Grid<Character> grille = new Grid<>(inputs, new Divider.Character());
+
+        for (int ligne = 0; ligne < grille.getHeight(); ligne++) {
+            for (int colonne = 0; colonne < grille.getWidth(); colonne++) {
+                if (grille.get(ligne, colonne) == 'X') {
+                    resultat += chercheXMAS(grille, ligne, colonne);
+                }
+            }
+        }
 
         System.out.println(this.getClass().getSimpleName() + " " + name + " : " + resultat);
         return resultat;
     }
 
+    public int traitement2(List<String> inputs) {
+        int resultat = 0;
+        Grid<Character> grille = new Grid<>(inputs, new Divider.Character());
+
+        for (int ligne = 0; ligne < grille.getHeight(); ligne++) {
+            for (int colonne = 0; colonne < grille.getWidth(); colonne++) {
+                if (grille.get(ligne, colonne) == 'A') {
+                    resultat += cherche2MAS(grille, ligne, colonne);
+                }
+            }
+        }
+
+        System.out.println(this.getClass().getSimpleName() + " " + name + " : " + resultat);
+        return resultat;
+    }
 }
