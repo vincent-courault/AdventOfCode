b9b1811c7bb844ac79b9f405e227281e71749d6b
Vincent <vincent.courault@gmail.com>
2024-12-06 07:23:03 +0100

    j5 et j6

--- 2024/src/test/java/adventofcode/AdventDay05.java
+++ 2024/src/test/java/adventofcode/AdventDay05.java
@@ -4,7 +4,8 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -14,32 +15,101 @@ public class AdventDay05 extends Commun {
     @Test
     public void etape1_exemple() throws URISyntaxException, IOException {
         List<String> inputs = lectureDuFichier(this, true);
-        assertEquals(0, traitement(inputs));
+        assertEquals(143, traitement(inputs, true));
     }
 
     @Test
     public void etape1() throws IOException, URISyntaxException {
         List<String> inputs = lectureDuFichier(this, false);
-        assertEquals(0, traitement(inputs));
+        assertEquals(4766, traitement(inputs, true));
     }
 
     @Test
     public void etape2_exemple() throws URISyntaxException, IOException {
         List<String> inputs = lectureDuFichier(this, true);
-        assertEquals(0, traitement(inputs));
+        assertEquals(123, traitement(inputs, false));
     }
 
     @Test
     public void etape2() throws IOException, URISyntaxException {
         List<String> inputs = lectureDuFichier(this, false);
-        assertEquals(0, traitement(inputs));
+        assertEquals(6257, traitement(inputs, false));
     }
 
-    public int traitement(List<String> inputs) {
-        int resultat = 0;
+    public int traitement(List<String> inputs, boolean etape1) {
+
+        Map<Integer, Page> rules = new HashMap<>();
+        List<String> pagesToProduce = new ArrayList<>();
+        for (String input : inputs) {
+            if (input.contains("|")) {
+                int[] in = Arrays.stream(input.split("\\|")).mapToInt(Integer::parseInt).toArray();
+                rules.putIfAbsent(in[0], new Page(in[0]));
+                rules.putIfAbsent(in[1], new Page(in[1]));
+                rules.get(in[0]).apres.add(in[1]);
+                rules.get(in[1]).avant.add(in[0]);
+            }
+            if (input.contains(",")) {
+                pagesToProduce.add(input);
+            }
+        }
+
+        int sommeDejaTrie = 0, sommeApresTri = 0, resultat = 0;
+        for (String s : pagesToProduce) {
+            int[] pages = Arrays.stream(s.split(",")).mapToInt(Integer::parseInt).toArray();
+            boolean ordered = true;
+
+            for (int page = 0; page < pages.length; page++) {
+                for (int pre = 0; pre < page; pre++) {
+                    if (rules.get(pages[page]).apres.contains(pages[pre])) {
+                        ordered = false;
+                    }
+                }
+                for (int post = page + 1; post < pages.length; post++) {
+                    if (rules.get(pages[page]).avant.contains(pages[post])) {
+                        ordered = false;
+                    }
+                }
+            }
+
+            if (ordered) {
+                sommeDejaTrie += pages[pages.length / 2];
+            } else {
+                List<Page> list = Arrays.stream(pages).mapToObj(rules::get).sorted().toList();
+                sommeApresTri += (list.get((list.size() - 1) / 2).page);
+            }
+        }
+
+        if (etape1) {
+            resultat = sommeDejaTrie;
+        } else {
+            resultat = sommeApresTri;
+        }
 
         System.out.println(this.getClass().getSimpleName() + " " + name + " : " + resultat);
+
         return resultat;
     }
 
+
+    static class Page implements Comparable<Page> {
+        int page;
+        List<Integer> avant = new ArrayList<>();
+        List<Integer> apres = new ArrayList<>();
+
+        public Page(int page) {
+            this.page = page;
+        }
+
+        @Override
+        public int compareTo(Page o) {
+            if (this.page == o.page) {
+                return 0;
+            }
+            if (this.avant.contains(o.page)) {
+                return 1;
+            } else {
+                return -1;
+            }
+        }
+    }
 }
--- 2024/src/test/java/adventofcode/AdventDay06.java
+++ 2024/src/test/java/adventofcode/AdventDay06.java
@@ -14,32 +14,174 @@ public class AdventDay06 extends Commun {
     @Test
     public void etape1_exemple() throws URISyntaxException, IOException {
         List<String> inputs = lectureDuFichier(this, true);
-        assertEquals(0, traitement(inputs));
+        assertEquals(41, traitement(inputs));
     }
 
     @Test
     public void etape1() throws IOException, URISyntaxException {
         List<String> inputs = lectureDuFichier(this, false);
-        assertEquals(0, traitement(inputs));
+        assertEquals(5404, traitement(inputs));
     }
 
     @Test
     public void etape2_exemple() throws URISyntaxException, IOException {
         List<String> inputs = lectureDuFichier(this, true);
-        assertEquals(0, traitement(inputs));
+        assertEquals(6, traitement2(inputs));
     }
 
     @Test
     public void etape2() throws IOException, URISyntaxException {
         List<String> inputs = lectureDuFichier(this, false);
-        assertEquals(0, traitement(inputs));
+        assertEquals(1984, traitement2(inputs));
     }
 
     public int traitement(List<String> inputs) {
         int resultat = 0;
+        Grid<Character> grille = new Grid<>(inputs, new Divider.Character());
+        Grid<Character> grilleVisitee = new Grid<>(inputs, new Divider.Character());
+        int gardeLigne = 0;
+        int gardeColonne = 0;
+        char sens = 'N';
+        for (int ligne = 0; ligne < grille.getHeight(); ligne++) {
+            for (int colonne = 0; colonne < grille.getWidth(); colonne++) {
+                if (grille.get(ligne, colonne) == '^') {
+                    gardeLigne = ligne;
+                    gardeColonne = colonne;
+                    grilleVisitee.set(gardeLigne, gardeColonne, '+');
+                    break;
+                }
+            }
+        }
 
+        while (grille.isValid(gardeLigne, gardeColonne)) {
+            if (grille.get(gardeLigne, gardeColonne) == '#') {
+                sens = switch (sens) {
+                    case 'N' -> {
+                        gardeLigne++;
+                        yield 'E';
+                    }
+                    case 'S' -> {
+                        gardeLigne--;
+                        yield 'O';
+                    }
+                    case 'E' -> {
+                        gardeColonne--;
+                        yield 'S';
+                    }
+                    case 'O' -> {
+                        gardeColonne++;
+                        yield 'N';
+                    }
+                    default -> sens;
+                };
+            } else {
+                grilleVisitee.set(gardeLigne, gardeColonne, '+');
+            }
+            switch (sens) {
+                case 'N':
+                    gardeLigne--;
+                    break;
+                case 'S':
+                    gardeLigne++;
+                    break;
+                case 'E':
+                    gardeColonne++;
+                    break;
+                case 'O':
+                    gardeColonne--;
+                    break;
+            }
+        }
+
+        for (int ligne = 0; ligne < grilleVisitee.getHeight(); ligne++) {
+            for (int colonne = 0; colonne < grilleVisitee.getWidth(); colonne++) {
+                if (grilleVisitee.get(ligne, colonne) == '+') {
+                    resultat++;
+                }
+            }
+        }
+        //System.out.println(grilleVisitee);
         System.out.println(this.getClass().getSimpleName() + " " + name + " : " + resultat);
+
         return resultat;
     }
 
+    public int traitement2(List<String> inputs) {
+        int resultat = 0;
+        Grid<Character> grille = new Grid<>(inputs, new Divider.Character());
+        Grid<Character> grilleVisitee = new Grid<>(inputs, new Divider.Character());
+        int gardeLigne = 0;
+        int gardeColonne = 0;
+        char sens = 'N';
+        for (int ligne = 0; ligne < grille.getHeight(); ligne++) {
+            for (int colonne = 0; colonne < grille.getWidth(); colonne++) {
+                if (grille.get(ligne, colonne) == '^') {
+                    gardeLigne = ligne;
+                    gardeColonne = colonne;
+                    grilleVisitee.set(gardeLigne, gardeColonne, '+');
+                    break;
+                }
+            }
+        }
+        int gardeLigneInit = gardeLigne;
+        int gardeColonneInit = gardeColonne;
+
+        for (int ligne = 0; ligne < grille.getHeight(); ligne++) {
+            for (int colonne = 0; colonne < grille.getWidth(); colonne++) {
+               if (grille.get(ligne, colonne) == '.') {
+                    grille.set(ligne, colonne, '#');
+                    int nb = 0;
+
+                    while (grille.isValid(gardeLigne, gardeColonne) && nb < 10000) {
+                        nb++;
+                        if (grille.get(gardeLigne, gardeColonne) == '#') {
+                            sens = switch (sens) {
+                                case 'N' -> {
+                                    gardeLigne++;
+                                    yield 'E';
+                                }
+                                case 'S' -> {
+                                    gardeLigne--;
+                                    yield 'O';
+                                }
+                                case 'E' -> {
+                                    gardeColonne--;
+                                    yield 'S';
+                                }
+                                case 'O' -> {
+                                    gardeColonne++;
+                                    yield 'N';
+                                }
+                                default -> sens;
+                            };
+                        }
+                        switch (sens) {
+                            case 'N':
+                                gardeLigne--;
+                                break;
+                            case 'S':
+                                gardeLigne++;
+                                break;
+                            case 'E':
+                                gardeColonne++;
+                                break;
+                            case 'O':
+                                gardeColonne--;
+                                break;
+                        }
+                    }
+
+                    if (grille.isValid(gardeLigne, gardeColonne)) {
+                        resultat++;
+                    }
+                    gardeLigne = gardeLigneInit;
+                    gardeColonne = gardeColonneInit;
+                    sens = 'N';
+                    grille.set(ligne, colonne, '.');
+                }
+            }
+        }
+        System.out.println(this.getClass().getSimpleName() + " " + name + " : " + resultat);
+        return resultat;
+    }
 }
