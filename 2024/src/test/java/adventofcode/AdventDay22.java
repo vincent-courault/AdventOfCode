0a0f27b07c0e21357da10048e95fbbd4cdd82299
Vincent <vincent.courault@gmail.com>
2024-12-23 14:45:40 +0100

    ajoute J22 et 23

--- 2024/src/test/java/adventofcode/AdventDay22.java
+++ 2024/src/test/java/adventofcode/AdventDay22.java
@@ -4,42 +4,97 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AdventDay22 extends Commun {
 
-
     @Test
     public void etape1_exemple() throws URISyntaxException, IOException {
         List<String> inputs = lectureDuFichier(this, true);
-        assertEquals(0, traitement(inputs));
+        assertEquals(37327623, traitement(inputs, false));
     }
 
     @Test
     public void etape1() throws IOException, URISyntaxException {
         List<String> inputs = lectureDuFichier(this, false);
-        assertEquals(0, traitement(inputs));
+        assertEquals(20401393616L, traitement(inputs, false));
     }
 
     @Test
     public void etape2_exemple() throws URISyntaxException, IOException {
-        List<String> inputs = lectureDuFichier(this, true);
-        assertEquals(0, traitement(inputs));
+        List<String> inputs = lectureDuFichier(this, true, 2);
+        assertEquals(23, traitement(inputs, true));
     }
 
     @Test
     public void etape2() throws IOException, URISyntaxException {
         List<String> inputs = lectureDuFichier(this, false);
-        assertEquals(0, traitement(inputs));
+        assertEquals(2272, traitement(inputs, true));
     }
 
-    public int traitement(List<String> inputs) {
-        int resultat = 0;
+    public long traitement(List<String> inputs, boolean etape2) {
+        long resultat = 0;
+        List<Long> nombresSecrets = inputs.stream().mapToLong(Long::parseLong).boxed().toList();
 
+        if (etape2) {
+            resultat = calculeSequencesEtSommesCorrespondantes(nombresSecrets);
+        } else {
+            for (Long nombreSecret : nombresSecrets) {
+                resultat += calculeNombreSecret(nombreSecret);
+            }
+        }
         System.out.println(this.getClass().getSimpleName() + " " + name + " : " + resultat);
         return resultat;
     }
 
+    private long calculeNombreSecret(long nombre) {
+        for (int i = 0; i < 2000; i++) {
+            nombre = calculeNombreSuivant(nombre);
+        }
+        return nombre;
+    }
+
+    private long calculeNombreSuivant(long nombreSecret) {
+
+        nombreSecret = (((nombreSecret << 6) ^ nombreSecret) & 0xFFFFFF);
+        nombreSecret = (((nombreSecret >> 5) ^ nombreSecret) & 0xFFFFFF);
+        nombreSecret = (((nombreSecret << 11) ^ nombreSecret) & 0xFFFFFF);
+        return nombreSecret;
+    }
+
+    private Map<String, Integer> calculeSequences(long nombreSecret) {
+        Map<String, Integer> result = new HashMap<>();
+        long nombrePrecedent = nombreSecret;
+        int prixPrecedent = (int) (nombreSecret % 10L);
+        int prix;
+        int diff;
+        long nombreSuivant;
+        List<Integer> collector = new ArrayList<>();
+        for (int i = 0; i < 2000; i++) {
+
+            nombreSuivant = calculeNombreSuivant(nombrePrecedent);
+            prix = (int) (nombreSuivant % 10);
+            diff = prix - prixPrecedent;
+            collector.add(diff);
+            if (collector.size() == 4) {
+                result.putIfAbsent(collector.toString(), prix);
+                collector.removeFirst();
+            }
+            prixPrecedent = prix;
+            nombrePrecedent = nombreSuivant;
+        }
+        return result;
+    }
+
+    public long calculeSequencesEtSommesCorrespondantes(List<Long> nombresSecrets) {
+        Map<String, Integer> mapDesPrix = new HashMap<>();
+        nombresSecrets.forEach(nombreSecret -> calculeSequences(nombreSecret).
+                forEach((key, value) -> mapDesPrix.merge(key, value, Integer::sum)));
+        return mapDesPrix.values().stream().mapToInt(Integer::intValue).max().orElseThrow();
+    }
 }
--- 2024/src/test/java/adventofcode/AdventDay23.java
+++ 2024/src/test/java/adventofcode/AdventDay23.java
@@ -4,42 +4,92 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AdventDay23 extends Commun {
-
 
     @Test
     public void etape1_exemple() throws URISyntaxException, IOException {
         List<String> inputs = lectureDuFichier(this, true);
-        assertEquals(0, traitement(inputs));
+        assertEquals("7", traitement(inputs, true));
     }
 
     @Test
     public void etape1() throws IOException, URISyntaxException {
         List<String> inputs = lectureDuFichier(this, false);
-        assertEquals(0, traitement(inputs));
+        assertEquals("1314", traitement(inputs, true));
     }
 
     @Test
     public void etape2_exemple() throws URISyntaxException, IOException {
-        List<String> inputs = lectureDuFichier(this, true);
-        assertEquals(0, traitement(inputs));
+        List<String> inputs = lectureDuFichier(this, true, 2);
+        assertEquals("co,de,ka,ta", traitement(inputs, false));
     }
 
     @Test
     public void etape2() throws IOException, URISyntaxException {
         List<String> inputs = lectureDuFichier(this, false);
-        assertEquals(0, traitement(inputs));
+        assertEquals("bg,bu,ce,ga,hw,jw,nf,nt,ox,tj,uu,vk,wp", traitement(inputs, false));
     }
 
-    public int traitement(List<String> inputs) {
-        int resultat = 0;
+    public String traitement(List<String> inputs, boolean etape1) {
+        String resultat;
+        Map<String, List<String>> connexions = new HashMap<>();
+        for (String input : inputs) {
+            String[] split = input.split("-");
+            if (!connexions.containsKey(split[0])) {
+                connexions.put(split[0], new ArrayList<>(Collections.singleton(split[1])));
+            } else {
+                connexions.get(split[0]).add(split[1]);
+            }
+            if (!connexions.containsKey(split[1])) {
+                connexions.put(split[1], new ArrayList<>(Collections.singleton(split[0])));
+            } else {
+                connexions.get(split[1]).add(split[0]);
+            }
+        }
+        if (etape1) {
+            resultat = String.valueOf(identifieLesTriplesInterconnectesCommencantParT(connexions).size());
+        } else {
+            Set<String> groupeLePlusLong = chercheLesGroupes(connexions).stream().max(Comparator.comparingInt(Set::size)).orElseThrow();
+            resultat = groupeLePlusLong.stream().sorted().collect(Collectors.joining(","));
+        }
 
         System.out.println(this.getClass().getSimpleName() + " " + name + " : " + resultat);
         return resultat;
     }
 
+    private Set<Set<String>> identifieLesTriplesInterconnectesCommencantParT(Map<String, List<String>> connexions) {
+        Set<Set<String>> triplesInterconnectes = new HashSet<>();
+        for (String ordinateur1 : connexions.keySet()) {
+            if (ordinateur1.startsWith("t")) {
+                connexions.get(ordinateur1)
+                        .forEach(ordinateur2 -> connexions.get(ordinateur2).stream()
+                                .filter(ordinateur3 -> !ordinateur3.equals(ordinateur1) && connexions.get(ordinateur3).contains(ordinateur1))
+                                .forEach(ordinateur3 -> triplesInterconnectes.add(Set.of(ordinateur1, ordinateur2, ordinateur3))));
+            }
+        }
+        return triplesInterconnectes;
+    }
+
+    private List<Set<String>> chercheLesGroupes(Map<String, List<String>> connexions) {
+        List<Set<String>> groupes = new ArrayList<>();
+        for (String ordinateur : connexions.keySet()) {
+            Set<String> groupe = new HashSet<>();
+            Queue<String> queue = new ArrayDeque<>();
+            queue.add(ordinateur);
+            while (!queue.isEmpty()) {
+                String ordinateurSuivant = queue.poll();
+                if (connexions.get(ordinateurSuivant).containsAll(groupe)) {
+                    groupe.add(ordinateurSuivant);
+                    queue.addAll(connexions.get(ordinateurSuivant));
+                }
+            }
+            groupes.add(groupe);
+        }
+        return groupes;
+    }
 }
