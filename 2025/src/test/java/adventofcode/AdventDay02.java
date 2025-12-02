606988116898f91198688a165fc151aee0eca0f3
Vincent <vincent.courault@gmail.com>
2025-12-02 07:41:14 +0100

    ajoute J2

--- 2025/src/test/java/adventofcode/AdventDay02.java
+++ 2025/src/test/java/adventofcode/AdventDay02.java
@@ -13,29 +13,68 @@ public class AdventDay02 extends Commun {
     @Test
     public void etape1_exemple() throws URISyntaxException, IOException {
         List<String> inputs = lectureDuFichier(this, true);
-        assertEquals(0, traitement(inputs));
+        assertEquals(1227775554L, traitement(inputs));
     }
 
     @Test
     public void etape1() throws IOException, URISyntaxException {
         List<String> inputs = lectureDuFichier(this, false);
-        assertEquals(0, traitement(inputs));
+        assertEquals(31839939622L, traitement(inputs));
     }
 
     @Test
     public void etape2_exemple() throws URISyntaxException, IOException {
         List<String> inputs = lectureDuFichier(this, true);
-        assertEquals(0, traitement(inputs));
+        assertEquals(4174379265L, traitement2(inputs));
     }
 
     @Test
     public void etape2() throws IOException, URISyntaxException {
         List<String> inputs = lectureDuFichier(this, false);
-        assertEquals(0, traitement(inputs));
+        assertEquals(41662374059L, traitement2(inputs));
     }
 
-    public int traitement(List<String> inputs) {
-        int resultat = 0;
+    public long traitement(List<String> inputs) {
+        long resultat = 0;
+        String[] plages = inputs.getFirst().split(",");
+        for (String plage : plages) {
+            String[] parts = plage.split("-");
+            long debut = Long.parseLong(parts[0]);
+            long fin = Long.parseLong(parts[1]);
+            // Pour chaque valeur de la plage, on coupe la chaine en 2 et on compare.
+            // Si la longueur est impaire, c'est forcément ko.
+            for (long valeur = debut; valeur <= fin; valeur++) {
+                String valeurATester = String.valueOf(valeur);
+                int longueur = valeurATester.length();
+                if (longueur % 2 == 0) {
+                    int milieu = valeurATester.length() / 2;
+                    if (valeurATester.substring(0, milieu).equals(valeurATester.substring(milieu))) {
+                        resultat += valeur;
+                    }
+                }
+            }
+        }
+        System.out.println(this.getClass().getSimpleName() + " " + name + " : " + resultat);
+        return resultat;
+    }
+
+    public long traitement2(List<String> inputs) {
+        long resultat = 0;
+        String[] plages = inputs.getFirst().split(",");
+
+        for (String plage : plages) {
+            String[] parts = plage.split("-");
+            long debut = Long.parseLong(parts[0]);
+            long fin = Long.parseLong(parts[1]);
+            // pour chaque valeur de la plage, on double la chaine et on enlève les bords
+            // et on vérifie la présence de la chaine d'origine
+            for (long valeur = debut; valeur <= fin; valeur++) {
+                String valeurATester = String.valueOf(valeur);
+                if ((valeurATester + valeurATester).substring(1, 2 * valeurATester.length() - 1).contains(valeurATester)) {
+                    resultat += valeur;
+                }
+            }
+        }
         System.out.println(this.getClass().getSimpleName() + " " + name + " : " + resultat);
         return resultat;
     }
